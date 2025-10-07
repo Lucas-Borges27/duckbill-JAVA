3b31293e9a04c1e174ea1ba45da13af874ac406b
Lucas Borges <lucasb.souza@icloud.com>
2025-10-07 18:42:43 -0300

    Adicionando Exception  GlobalExceptionHandler to manage DataIntegrityViolationException

---
+++ src/main/java/com/db/duckbill/web/controller/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.db.duckbill.web.controller;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Dados duplicados ou violação de restrição: " + ex.getMessage());
+    }
+}
